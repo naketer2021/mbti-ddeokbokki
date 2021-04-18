8c916f0c9106ea7c122de123ba32c5d7ac8d73d3
naketer2021 <yn.kim921@gmail.com>
2021-04-18 23:55:57 +0900

    img

--- mbti.js
+++ mbti.js
@@ -90,22 +90,22 @@ var testNum = {
 
 //결과
 var result = {
-  'INTJ': {'img':'<img src="http://imagescdn.gettyimagesbank.com/500/201811/a11221752.jpg" class="mt-5" style="width: 300px;">', 'mbti': 'INTJ', 'explain': '떡볶이의 새로운 비전 크림떡볶이'},
-  'INTP': {'img':'<img src="https://www.yupdduk.com/images/menu/2020_hotmenuY01.png" class="mt-5" style="width: 300px;">','mbti': 'INTP', 'explain': '떡볶이계의 매콤한 비평가 동대문떡볶이'},
-  'ENTJ': {'img':'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyA4fgWtu6FdiWZt36TzCwl1uoTpD3tpwDCYMnw7RVTfXEOFuxrLwi5SgqBo1iL9IUHws&usqp=CAU "class="mt-5" style="width: 300px;">','mbti': 'ENTJ', 'explain': '학교 앞 대장 분식집 떡볶이'},
-  'ENTP': {'img':'<img src="https://ssproxy.ucloudbiz.olleh.com/v1/AUTH_e59809eb-bdc9-44d7-9d8f-2e7f0e47ba91/post_card/74709_1608102130_aqBw8y0I.png" class="mt-5" style="width: 300px;"">','mbti': 'ENTP', 'explain': '획기적인 발명가! 로제떡볶이'},
-  'INFJ': {'img':'<img src="https://imagescdn.gettyimagesbank.com/500/201811/a11203592.jpg" style="width: 300px;">','mbti': 'INFJ', 'explain': '투명한듯 칼칼한 국물떡볶이'},
-  'INFP': {'img':'<img src="https://recipe1.ezmember.co.kr/cache/recipe/2017/05/10/1b6d4020fc0c24370a624eddae427cdf1.jpg" class="mt-5"  style="width: 300px;">','mbti': 'INFP', 'explain': '맵지 않아도 괜찮아. 남녀노소 궁중떡볶이'},
-  'ENFJ': {'img':'<img src="https://lh3.googleusercontent.com/proxy/W_vEyT8rCMiflHkrm1wB6u2AH1o9ZyOoj6ww6mfzIPn28x0UVo79W_GyYI64KcxaVkUsbXRUoSzjokSc4aVy78OKMQQMbulbZTCk3f5kxyeh2vXHHGM" class="mt-5" style="width: 300px;">','mbti': 'ENFJ', 'explain': '매끄러운 언변, 누들떡볶이'},
-  'ENFP': {'img':'<img src="https://recipe1.ezmember.co.kr/cache/recipe/2018/03/04/ba55307500f38b5b93e75f479290cd711.jpg" class="mt-5" style="width: 300px;">','mbti': 'ENFP', 'explain': '떡볶이에 왜 떡이 꼭 있어야 돼? 라볶이'},
-  'ISTJ': {'img':'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPonbxDCPkEU3xhjPuEL-7g36P7dhscvwbkA&usqp=CAU" class="mt-5" style="width: 300px;">','mbti': 'ISTJ', 'explain': '클래식한 떡볶이의 정석 기름떡볶이'},
-  'ISFJ': {'img':'<img src="https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20210402033645688_photo_5ad86b50a6d2.jpg" class="mt-5" style="width: 300px;">','mbti': 'ISFJ', 'explain': '묵묵하고 꾸준한 가래떡 떡볶이'},
-  'ESTJ': {'img':'<img src="https://lh3.googleusercontent.com/proxy/oGxEg6HCrME0s-B8w8r4jWhuPwLNxOE_Y2fg8h9ct4OykWk-7IxMJ1azGNdnGKQT34zwU03r0Fl7nZeKOc6EG0MvFetEGsyYObpo8s9uDYLHaA" class="mt-5" style="width: 300px;">','mbti': 'ESTJ', 'explain': '실용적인 프랜차이즈, 상어떡볶이'},
-  'ESFJ': {'img':'<img src="https://cdn.crowdpic.net/list-thumb/thumb_l_E54128D72667B788B6899908E91CF139.jpg" class="mt-5" style="width: 300px;">','mbti': 'ESFJ', 'explain': '학교 끝나고 같이 놀래? 컵볶이'},
-  'ISTP': {'img':'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3Ad7JxGVgODuhjuq4WBi7EG6GErdYGDi5Q&usqp=CAU" class="mt-5" style="width: 300px;">','mbti': 'ISTP', 'explain': '어디에나 잘 어울리는, 짜장떡볶이'},
-  'ISFP': {'img':'<img src="https://t1.daumcdn.net/cfile/blog/2566044658A4FDA832" class="mt-5" style="width: 300px;">','mbti': 'ISFP', 'explain': '삼삼하지만 종종 생각나는, 홈메이드 떡볶이'},
-  'ESTP': {'img':'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3Ad7JxGVgODuhjuq4WBi7EG6GErdYGDi5Q&usqp=CAU" class="mt-5" style="width: 300px;">','mbti': 'ESTP', 'explain': '떡볶이도 먹고 싶고 피카츄도 먹고 싶은 나는? 떡꼬치'},
-  'ESFP': {'img':'<img src="https://imagescdn.gettyimagesbank.com/500/201709/jv11003662.jpg" class="mt-5" style="width: 300px;">','mbti': 'ESFP', 'explain': '우리는 모두 친구 즉석떡볶이'},
+  'INTJ': {'img':'<img src="img/cream.png" class="mt-5" style="width: 300px;">', 'mbti': 'INTJ', 'explain': '떡볶이의 새로운 비전 크림떡볶이'},
+  'INTP': {'img':'<img src="img/hothot.png" class="mt-5" style="width: 300px;">','mbti': 'INTP', 'explain': '떡볶이계의 매콤한 비평가 동대문떡볶이'},
+  'ENTJ': {'img':'<img src="img/school.png"class="mt-5" style="width: 300px;">','mbti': 'ENTJ', 'explain': '학교 앞 대장 분식집 떡볶이'},
+  'ENTP': {'img':'<img src="img/rose.png" class="mt-5" style="width: 300px;"">','mbti': 'ENTP', 'explain': '획기적인 발명가! 로제떡볶이'},
+  'INFJ': {'img':'<img src="img/soup.png" style="width: 300px;">','mbti': 'INFJ', 'explain': '투명한듯 칼칼한 국물떡볶이'},
+  'INFP': {'img':'<img src="img/royal.png" class="mt-5"  style="width: 300px;">','mbti': 'INFP', 'explain': '맵지 않아도 괜찮아. 남녀노소 궁중떡볶이'},
+  'ENFJ': {'img':'<img src="img/noodle.png" class="mt-5" style="width: 300px;">','mbti': 'ENFJ', 'explain': '매끄러운 언변, 누들떡볶이'},
+  'ENFP': {'img':'<img src="img/ramyun.png" class="mt-5" style="width: 300px;">','mbti': 'ENFP', 'explain': '떡볶이에 왜 떡이 꼭 있어야 돼? 라볶이'},
+  'ISTJ': {'img':'<img src="img/oil.png" class="mt-5" style="width: 300px;">','mbti': 'ISTJ', 'explain': '클래식한 떡볶이의 정석 기름떡볶이'},
+  'ISFJ': {'img':'<img src="img/ricedeock.png" class="mt-5" style="width: 300px;">','mbti': 'ISFJ', 'explain': '묵묵하고 꾸준한 가래떡 떡볶이'},
+  'ESTJ': {'img':'<img src="img/jaws.png" class="mt-5" style="width: 300px;">','mbti': 'ESTJ', 'explain': '실용적인 프랜차이즈, 상어떡볶이'},
+  'ESFJ': {'img':'<img src="img/cup.png" class="mt-5" style="width: 300px;">','mbti': 'ESFJ', 'explain': '학교 끝나고 같이 놀래? 컵볶이'},
+  'ISTP': {'img':'<img src="img/jjajang.png" class="mt-5" style="width: 300px;">','mbti': 'ISTP', 'explain': '어디에나 잘 어울리는, 짜장떡볶이'},
+  'ISFP': {'img':'<img src="img/hommade.png" class="mt-5" style="width: 300px;">','mbti': 'ISFP', 'explain': '삼삼하지만 종종 생각나는, 홈메이드 떡볶이'},
+  'ESTP': {'img':'<img src="img/stick.png" class="mt-5" style="width: 300px;">','mbti': 'ESTP', 'explain': '떡볶이도 먹고 싶고 피카츄도 먹고 싶은 나는? 떡꼬치'},
+  'ESFP': {'img':'<img src="img/everything.png" class="mt-5" style="width: 300px;">','mbti': 'ESFP', 'explain': '우리는 모두 친구 즉석떡볶이'},
 };
 
 //테스트시작
